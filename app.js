8152b6590738c063e5dad303c993cebc21c54eea
symien moore <symien.moore@gmail.com>
2021-03-25 20:51:53 -0400

    basic config

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
---
+++ controllers/user.controller.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+exports.registerUser = (req, res) => {
+
+}
+
+exports.doUserLogin = (req, res) => {
+    
+}
