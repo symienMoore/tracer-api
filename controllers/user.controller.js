a761d7bd0b28d86af9de2e6dd167ca0e73790086
symien moore <symien.moore@gmail.com>
2021-03-28 07:57:17 -0400

    hashed passwords

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,22 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const validation = ('./validation.js');
+const jwt    = require('jsonwebtoken');
+const User   = require('../models/User');
+const bcrypt = require('bcrypt');
 
 
 exports.registerUser = async (req, res) => {
     // runs validation on passed in data
     doRegisterCheck(req.body, req, res);
     // uses passed in data to check for user
-    const invalidEmail = User.findOne({ email: req.body.email });
+    const invalidEmail = await User.findOne({ email: req.body.email });
     if (invalidEmail) return res.status(400).send('It appears the user exists');
     // if no user and request is valid
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
     const user = new User({
         name: req.body.name,
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hash
     });
     try {
         await user.save();
@@ -29,7 +31,7 @@ exports.doUserLogin = (req, res) => {
 }
 
 
-//VALIDATORS
+//VALIDATORS && HASHER
 doRegisterCheck = (data, req, res) => {
     if (data.password.length < 6) {
           res.send('sorry, password must be at least 6 characters');
@@ -38,5 +40,4 @@ doRegisterCheck = (data, req, res) => {
     } else if (!data.email.includes('@') || !data.email.includes('.')) {
           res.send('please enter a valid email address');
     }
-
 };
